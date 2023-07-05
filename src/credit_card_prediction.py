685ac7befaaac4a0879eb6b2f32f6aae5dd56da1
kmaengggong <kmaengggong@gmail.com>
2023-07-05 14:13:05 +0900

    김명호: [230705] Nan > 라벨링 -> 라벨링 > NaN

--- src/credit_card_prediction.py
+++ src/credit_card_prediction.py
@@ -14,6 +14,7 @@ import time, datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.impute import SimpleImputer
 
 # DL
 import pandas as pd
@@ -35,39 +36,46 @@ from sklearn.tree import DecisionTreeClassifier
 data_path = "./data/credit_card_prediction/"
 datasets = pd.read_csv(data_path + "train.csv")
 
-# 1.1 NaN값 처리 - 최빈값으로 처리
-datasets = datasets.fillna(datasets["Credit_Product"].value_counts().idxmax())
-
-string_columns = ["ID", "Gender", "Region_Code", "Occupation", "Channel_Code", "Credit_Product", "Is_Active"]
-label_encoder = LabelEncoder()
-for i in range(len(string_columns)):
-    datasets[string_columns[i]] = label_encoder.fit_transform(datasets[string_columns[i]])
-
-x = datasets.drop(columns="Is_Lead")
-y = datasets.Is_Lead
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6, test_size=0.2, random_state=72, shuffle=True)
-
 # 1-1. 데이터 확인
 # Columns
 # ['ID', 'Gender', 'Age', 'Region_Code', 'Occupation', 'Channel_Code', 'Vintage', 'Credit_Product', 'Avg_Account_Balance', 'Is_Active', 'Is_Lead']
 # Target = Is_Lead, value=[0, 1]
 # print(datasets.shape)  # (245725, 11)
 
-# 1-2. 스케일링
-scaler = StandardScaler()
+# 1-2. 라벨링
+string_columns = ["ID", "Gender", "Region_Code", "Occupation", "Channel_Code", "Credit_Product", "Is_Active"]
+label_encoder = LabelEncoder()
+for i in range(len(string_columns)):
+    datasets[string_columns[i]] = label_encoder.fit_transform(datasets[string_columns[i]])
+
+# 1.3 NaN값 처리 - 최빈값으로 처리
+# datasets = datasets.fillna(datasets["Credit_Product"].value_counts().idxmax())
+x = datasets.drop(columns=["Is_Lead", "ID"])
+# imputer = SimpleImputer()  # 평균값
+# imputer = SimpleImputer(strategy="mean")  # default
+# imputer = SimpleImputer(strategy="median")  # 중간값
+imputer = SimpleImputer(strategy="most_frequent")  # 최빈값
+imputer.fit(x)
+x = imputer.transform(x)
+
+y = datasets.Is_Lead
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6, test_size=0.2, random_state=7211, shuffle=True)
+
+# 1-4. 스케일링
+scaler = StandardScaler()  # yet best
 # scaler = RobustScaler()
 # scaler = MinMaxScaler()
 # scaler = MaxAbsScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-# 1-3. 데이터 시각화
+# 1-5. 데이터 시각화 (Heatmap)
 # sns.set(font_scale=1.2)
 # sns.set(rc = {"figure.figsize":(12, 9)})
 # sns.heatmap(data=datasets.corr(), square=True, annot=True, cbar=True)
 # plt.show()
 
-# 1-4. Outlier
+# 1-6. Outlier
 # outlers = np.array([
 #         -50, -10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 #         11, 12, 13, 14, 15, 16, 17, 18, 50, 100
@@ -103,13 +111,9 @@ x_test = scaler.transform(x_test)
 
 # 2. 모델 구성
 # DL
-model = Sequential()
-model.add(Dense(128, input_dim=10))
-model.add(Dense(64))
-model.add(Dense(32))
-model.add(Dense(64))
-model.add(Dense(128))
-model.add(Dense(1, activation="sigmoid"))
+# model = Sequential()
+# model.add(Dense(128, input_dim=9, activation="relu"))
+# model.add(Dense(1, activation="sigmoid"))
 
 # ML
 # model = SVC()
@@ -119,18 +123,18 @@ model.add(Dense(1, activation="sigmoid"))
 
 # 3. 컴파일, 훈련
 # DL
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+# model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-early_stopping = EarlyStopping(
-    monitor="val_loss",
-    mode="min",
-    patience=10,
-    restore_best_weights=True
-)
+# early_stopping = EarlyStopping(
+#     monitor="val_loss",
+#     mode="min",
+#     patience=10,
+#     restore_best_weights=True
+# )
 
-start_time = time.time()
-model.fit(x_train, y_train, epochs=100, batch_size=128, validation_split=0.2, callbacks=[early_stopping])
-end_time = time.time()
+# start_time = time.time()
+# model.fit(x_train, y_train, epochs=500, batch_size=128, validation_split=0.2, callbacks=[early_stopping])
+# end_time = time.time()
 
 # ML
 # start_time = time.time()
@@ -139,17 +143,17 @@ end_time = time.time()
 
 # 4. 평가, 예측
 # DL
-loss, acc = model.evaluate(x_test, y_test)
-print("loss:", loss)
-print("acc:", acc)
-print("time:", end_time-start_time)
+# loss, acc = model.evaluate(x_test, y_test)
+# print("loss:", loss)
+# print("acc:", acc)
+# print("time:", end_time-start_time)
 
 # ML
 # score = cross_val_score(model, x, y, cv=kfold)
 # print("acc:", round(np.mean(score), 4))
 # print("time:", end_time-start_time)
 
-# 4-1. Feature Importances 시각화(ML)
+# 4-1. 데이터 시각화 (Feature Importances, ML)
 # n_features = x.shape[1]
 # plt.barh(range(n_features), model.feature_importances_, align="center")
 # plt.yticks(np.arange(n_features), x.columns)
@@ -161,11 +165,11 @@ print("time:", end_time-start_time)
 
 
 # 5. 결과
-# DL
-# DL / StandardScaler / 128 64 32 64 128 1 / epochs=100, batch_size=128
-# loss: 0.5007683038711548
-# acc: 0.7551124095916748
-# time: 65.36959648132324
+## DL / SI(최빈) / Drop["Id"] / rs=7211 / StandardScaler / 128(relu) 8 16 32 64 128 256 512(relu) 256 128 64 32 16 8 1 / epochs=100, batch_size=128 / ES=10
+# loss: 0.342851459980011
+# acc: 0.8620002269744873
+# time: 337.30227875709534
+
 
 # ML
 # ML / StandardScaler / DecisionTreeClassifier
